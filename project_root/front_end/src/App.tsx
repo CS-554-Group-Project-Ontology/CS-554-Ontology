5a948d5d0cb3f858efe11d834ae014c9264b1ea1
Cedric Pollefeys <cedric.pollefeys@gmail.com>
2026-03-29 11:54:26 -0400

    create basic landing and not found

--- project_root/front_end/src/App.tsx
+++ project_root/front_end/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <>
-      <h1>Ontology</h1>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
