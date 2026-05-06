bf247c192dd0551f3bd14cd8a0290ee7a8df58f0
Cedric Pollefeys <cedric.pollefeys@gmail.com>
2026-05-06 15:24:42 -0400

    fix sign-up redirect to existing route

--- project_root/front_end/src/pages/SignUp.tsx
+++ project_root/front_end/src/pages/SignUp.tsx
@@ -56,7 +56,7 @@ function SignUp() {
       await doCreateUserWithEmailAndPassword(email, password, displayName);
       await addUserApi();
       await refreshUser();
-      navigate('/dashboard');
+      navigate('/');
     } catch (err) {
       setError((err as Error).message);
       setSigningUp(false);
